7f575a8591e47cb86c95326efaef83eb61de6416
elitniy <elitniy.07@gmail.com>
2018-04-03 19:00:19 +0400

    duzeliwler

--- js.js
+++ js.js
@@ -1,45 +1,66 @@
 var rotation=45;
 var margin=80;
 var count=0;
+var radius=0;
+
+var box=document.querySelector(".box");
 function kruti(){
-	// var x = Math.floor(Math.random() * 279);
- //    var y = Math.floor(Math.random() * 279);
- //    var z = Math.floor(Math.random() * 279);
- //    var bgColor = "rgb(" + x/2 + "," + y + "," + z + ")";  
-	  var box=document.querySelector(".box");
-	// if (count=1) {
-	// 		box.setAttribute("style","border-radius: 0 0 15px 0;");
-	//  }
-	//  else if(count=2){
-	//  	box.setAttribute("style","border-radius: 0  0 0 15px;");
-	//  }
-	// else if(count=3){
-	// 	box.setAttribute("style","border-radius:15px 0 0 0;");
-	// }
-	// else if(count=4){
-	// 	box.setAttribute("style","border-radius: 0 15px 0 0 ;");
-	// }
-	switch(count) {
-    // case 0:
-    //     box.setAttribute("style","border-radius:0 0 15px 0;");
-     // case 2:
-     //     box.setAttribute("style","border-radius:0 15px 0  0 ;");
-     // case 4:
-     //     box.setAttribute("style","border-radius:15px 0 0 0;");
-    case 6:
-        box.setAttribute("style","border-radius: 0  0 0 15px ;");
-	}
 	box.style.transform="rotate("+rotation+"deg)";
-	box.style.backgroundColor= "green";
+	box.style.transformOrigin="center center";
 	box.style.transition= 0.8+"s";
 	box.style.marginLeft= margin+"px";
+	if(count==0){
+		radius=25;
+		box.style.borderRadius="0px 0px "+radius+"px 0px"
+		if(count==2){
+		radius=25;
+		box.style.borderRadius="0px "+radius+"px 0px 0px"
+		}
+		if(count==4){
+		radius=25;
+		box.style.borderRadius=""+radius+"px 0px 0px 0px"
+		}
+		if(count==6){
+		radius=25;
+		box.style.borderRadius="0px 0px 0px "+radius+"px"
+		}
+	}else{
+		radius=0;
+	}
+	count++;
 	rotation+=45;
 	margin+=40;
-	count+=1;
-	console.log(count);
-
-	if(count>=6){
-		count=0;
-	}
 }
 var stop=setInterval(kruti,1000);
+
+
+
+
+
+// var rotation=45;
+// var margin=80;
+// var count=0;
+// var radius=0;
+// var box=document.querySelector(".box");
+
+// function kruti(){
+
+// 	box.style.transform="rotate("+rotation+"deg)";
+// 	box.style.transformOrigin="center center";
+// 	box.style.transition= 0.8+"s";
+// 	box.style.marginLeft= margin+"px";
+	
+	
+// 	if(count>=6){
+// 		radius=50;
+// 		count=0;
+// 	}else{
+// 		radius=0;
+// 	}
+// 	box.style.borderRadius=""+radius+"px 0px 0px 0px"
+// 	count++;
+// 	rotation+=45;
+// 	margin+=40;
+
+// }
+// var stop=setInterval(kruti,1000);
